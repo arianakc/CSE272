da4decf0b91fb6eb16226237984cb71fe391a7a4
Frank <cli237@ucsc.edu>
2021-05-29 19:39:11 -0400

    remove redundant code

--- project/download_model.sh
+++
@@ -1,5 +0,0 @@
-mkdir model
-cd model
-wget https://huggingface.co/bert-base-multilingual-cased/resolve/main/config.json
-wget https://huggingface.co/bert-base-multilingual-cased/resolve/main/pytorch_model.bin
-wget https://huggingface.co/bert-base-multilingual-cased/resolve/main/vocab.txt
