f5087e7f4b881f89c077c67875d3474bd30ecb58
Frank <cli237@ucsc.edu>
2021-05-30 08:48:47 -0400

    remove comment of the data process code

--- project/preprocess.py
+++ project/preprocess.py
@@ -70,8 +70,8 @@ if __name__ == '__main__':
     ar_doc_out_path = "data/ar.documents.tsv"
     zh_doc_in_path = "data/zh.tsv"
     zh_doc_out_path = "data/zh.documents.tsv"
-    # generate_docs(ar_doc_in_path, ar_doc_out_path)
-    # generate_docs(zh_doc_in_path, zh_doc_out_path)
+    generate_docs(ar_doc_in_path, ar_doc_out_path)
+    generate_docs(zh_doc_in_path, zh_doc_out_path)
     en_ar_train = "data/en.ar.train.jl"
     en_ar_dev = "data/en.ar.dev.jl"
     en_ar_test = "data/en.ar.test1.jl"
@@ -80,23 +80,23 @@ if __name__ == '__main__':
     en_zh_test = "data/en.zh.test1.jl"
     en_ar_queries = "data/en.ar.queries.tsv"
     en_zh_queries = "data/en.zh.queries.tsv"
-    # generate_queries(en_ar_train, en_ar_dev, en_ar_test, en_ar_queries)
-    # generate_queries(en_zh_train, en_zh_dev, en_zh_test, en_zh_queries)
+    generate_queries(en_ar_train, en_ar_dev, en_ar_test, en_ar_queries)
+    generate_queries(en_zh_train, en_zh_dev, en_zh_test, en_zh_queries)
     en_ar_train_pairs = "data/en.ar.train.pairs"
     en_zh_train_pairs = "data/en.zh.train.pairs"
-    # generate_pairs(en_ar_train, en_ar_train_pairs)
-    # generate_pairs(en_zh_train, en_zh_train_pairs)
+    generate_pairs(en_ar_train, en_ar_train_pairs)
+    generate_pairs(en_zh_train, en_zh_train_pairs)
     en_ar_qrels = "data/en.ar.qrels"
     en_zh_qrels = "data/en.zh.qrels"
-    # generate_trec_qrels(en_ar_train, en_ar_dev, en_ar_test, en_ar_qrels)
+    generate_trec_qrels(en_ar_train, en_ar_dev, en_ar_test, en_ar_qrels)
     generate_trec_qrels(en_zh_train, en_zh_dev, en_zh_test, en_zh_qrels)
     en_ar_dev_run = "data/en.ar.dev.run"
     en_zh_dev_run = "data/en.zh.dev.run"
     en_ar_test_run = "data/en.ar.test.run"
     en_zh_test_run = "data/en.zh.test.run"
-    # generate_trec_run(en_ar_dev, en_ar_dev_run)
-    # generate_trec_run(en_ar_test, en_ar_test_run)
-    # generate_trec_run(en_zh_dev, en_zh_dev_run)
-    # generate_trec_run(en_zh_test, en_zh_test_run)
+    generate_trec_run(en_ar_dev, en_ar_dev_run)
+    generate_trec_run(en_ar_test, en_ar_test_run)
+    generate_trec_run(en_zh_dev, en_zh_dev_run)
+    generate_trec_run(en_zh_test, en_zh_test_run)
     print("Finished!")
 
